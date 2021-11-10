99cd871f9ae229841f8bb5504e2246297d51cb2d
Alex Mihailus <mihailus.a.g@gmail.com>
2021-11-10 21:55:23 +0200

    add structure in the database for likes and their display in the personal account

--- app/Http/Controllers/Personal/Main/IndexController.php
+++ app/Http/Controllers/Personal/Main/IndexController.php
@@ -3,10 +3,6 @@
 namespace App\Http\Controllers\Personal\Main;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\Tag;
-use App\Models\User;
 
 class IndexController extends Controller
 {
